202ccb8045062b28ce456b4134f1e79cbe3f6bda
mk-petrov <petko.petrov89@gmail.com>
2018-10-16 23:52:40 +0200

    footer component done

--- movie_DB/source/client/components/App.js
+++ movie_DB/source/client/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <Navbar history={this.props.history} />
+        <Navbar />
         {this.props.children}
+        <Footer />
       </div>
     )
   }
