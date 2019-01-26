3068f703e3f7e5cd4b5c929ec999b04ee6cab132
mk-petrov <petko.petrov89@gmail.com>
2019-01-26 13:18:31 +0200

    side drawer added

--- BurgerBuilder app/src/components/Layout/Layout.js
+++ BurgerBuilder app/src/components/Layout/Layout.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxiliary'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 import classes from './Layout.css' // import a classes object with css props
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-    {/* use the main as a wrapper for the core component we want to show */}
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-)
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
 
-export default layout
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      // the clear way to change the state when it's related to the previous state
+      return {showSideDrawer: !prevState.showSideDrawer}
+    })
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
+
+export default Layout
